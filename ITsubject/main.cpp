0e24e93b71ca14a6e08b999e499e193f9b72b2e2
Martin0316 <KZY@malideimac.local>
2017-09-25 21:54:53 +0800

    简化版

--- ITsubject/main.cpp
+++ ITsubject/main.cpp
@@ -9,16 +9,21 @@
 #include <iostream>
 using namespace std;
 int FristNumber,SecondNumber,ThirdNumber;
+int  a,b,c,d;
 int main() {
     // insert code here...
-    cout << "This program counts three numbers. \n";
-    cout << "Input frist number:";
-    cin >> FristNumber;
-    cout << "Input Second number:";
-    cin >> SecondNumber;
-    cout << "Input Third number:";
-    cin >> ThirdNumber;
-    cout << "The sum is " << FristNumber+SecondNumber+ThirdNumber << endl ;
+    
+    cin >> a >> b>> c;
+     //d = a + b + c;
+    cout << a+b+c << endl;
+    //cout << "This program counts three numbers. \n";
+    //cout << "Input frist number:";
+    //cin >> FristNumber;
+    //cout << "Input Second number:";
+    //cin >> SecondNumber;
+   //cout << "Input Third number:";
+    //cin >> ThirdNumber;
+    //cout << "The sum is " << FristNumber+SecondNumber+ThirdNumber << endl ;
     
     return 0;
     
